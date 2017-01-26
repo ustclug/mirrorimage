1bb714399181e1d2f7d0eefb24476530a47d5185
Yifan Gao <git@gaoyifan.com>
2017-01-26 10:23:34 +0800

    more accurate has_modified evaluation

--- build.sh
+++ build.sh
@@ -2,7 +2,10 @@
 
 has_modified(){
     file=$1
-    [[ `git diff HEAD~ HEAD -- $file | wc -l` != 0 ]]
+    [[ -z $TRAVIS_COMMIT_RANGE ]] && return false
+    COMMIT_FROM=${TRAVIS_COMMIT_RANGE%...*}
+    COMMIT_TO=${TRAVIS_COMMIT_RANGE#*...}
+    [[ `git diff $COMMIT_FROM $COMMIT_TO -- $file | wc -l` != 0 ]]
 }
 
 script=build-${IMAGE_NAME}.sh
