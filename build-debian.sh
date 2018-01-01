5b803e8d3170ccc89d9968890e1c042bcd01cfb1
Yifan Gao <git@yfgao.com>
2018-01-01 14:03:10 +0000

    filter debian image tag

--- build-debian.sh
+++ build-debian.sh
@@ -3,6 +3,7 @@ source common.sh
 docker-tags debian | 
 while read tag; do
     unset backports_list
+    echo $tag | grep -qP -- '-\d{8}' && continue
     echo $tag | grep backports && backports_list=/etc/apt/sources.list.d/backports.list
     rocker build --push --auth $DOCKER_USER:$DOCKER_PASS -f <(cat << EOF
 FROM debian:$tag
