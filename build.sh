70f1c6cc15d6420778d65d3e585cb747dd8e8d55
taoky <taoky1999@gmail.com>
2020-12-26 15:15:41 +0800

    Update Actions script

--- build.sh
+++ build.sh
@@ -2,14 +2,11 @@
 
 has_modified(){
     file=$1
-    [[ -z $TRAVIS_COMMIT_RANGE ]] && return false
-    COMMIT_FROM=${TRAVIS_COMMIT_RANGE%...*}
-    COMMIT_TO=${TRAVIS_COMMIT_RANGE#*...}
     [[ `git diff $COMMIT_FROM $COMMIT_TO -- $file | wc -l` != 0 ]]
 }
 
 script=build-${IMAGE_NAME}.sh
-if has_modified $script || [[ $TRAVIS_EVENT_TYPE == "cron" ]] || true ; then
+if has_modified $script || [[ $GITHUB_EVENT == "schedule" ]] || true ; then
     docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
     . $script
 fi
