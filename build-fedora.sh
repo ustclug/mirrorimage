5e8699e650b1b0f5ae74eefbd51fb8684a8f9f2a
taoky <me@taoky.moe>
2023-12-02 22:21:24 +0800

    fedora: fix build error on Fedora 39+
    See:
    https://fedoraproject.org/wiki/Changes/No_default_fedora-repos-modular

--- build-fedora.sh
+++ build-fedora.sh
@@ -3,15 +3,13 @@ source common.sh
 docker-tags fedora |
 while read tag; do
     dockerfile=$(mktemp)
+    sedcommand="sed -e 's|^metalink=|#metalink=|g' \
+        -e 's|^#baseurl=http://download.example/pub/fedora/linux|baseurl=https://mirrors.ustc.edu.cn/fedora|g' \
+        -i.bak "
     cat << EOF > $dockerfile
 FROM fedora:$tag
-RUN sed -e 's|^metalink=|#metalink=|g' \
-        -e 's|^#baseurl=http://download.example/pub/fedora/linux|baseurl=https://mirrors.ustc.edu.cn/fedora|g' \
-        -i.bak \
-        /etc/yum.repos.d/fedora.repo \
-        /etc/yum.repos.d/fedora-modular.repo \
-        /etc/yum.repos.d/fedora-updates.repo \
-        /etc/yum.repos.d/fedora-updates-modular.repo
+RUN $sedcommand /etc/yum.repos.d/fedora-modular.repo /etc/yum.repos.d/fedora-updates-modular.repo || true \
+    && $sedcommand /etc/yum.repos.d/fedora.repo /etc/yum.repos.d/fedora-updates.repo     
 EOF
     docker build -f $dockerfile -t ustclug/fedora:$tag .
     docker push ustclug/fedora:$tag
