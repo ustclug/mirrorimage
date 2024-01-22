846d3afaa01fe9bacf87478a10bfbab76f1e2055
scsc <754396291@qq.com>
2024-01-23 01:55:05 +0800

    Add quotes to prevent word splitting and other optimizations

--- common.sh
+++ common.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-one_year_ago=$(date -d "1 year ago" +%s)
+one_year_ago="$(date -d "1 year ago" +%s)"
 parse_tags_js() {
     is_empty=true
-    page_url=$(jq -r '.next' <(echo "$tags_js"))
-    for result in $(jq -c '.results[]' <(echo "$tags_js")); do
-        if ! date=$(date -d "$(jq -r '.last_updated' <(echo "$result"))" +%s); then
+    page_url="$(jq -r '.next' <<< "$tags_js")"
+    for result in $(jq -c '.results[]' <<< "$tags_js"); do
+        if ! date="$(date -d "$(jq -r '.last_updated' <<< "$result")" +%s)"; then
             return 1
         fi
-        if ! name=$(jq -r '.name' <(echo "$result")); then
+        if ! name="$(jq -r '.name' <<< "$result")"; then
             return 1
         fi
         if (( date > one_year_ago )); then
@@ -18,11 +18,11 @@ parse_tags_js() {
     done
 }
 docker-tags(){
-    image=library/$1
+    image="library/$1"
     page_url="https://registry.hub.docker.com/v2/repositories/${image}/tags/"
-    while [[ -n "$page_url" && "$page_url" != "null" && $is_empty != true ]]
+    while [[ -n "$page_url" && "$page_url" != "null" && "$is_empty" != "true" ]]
     do
-        if ! tags_js=$(curl -sSL "$page_url"); then
+        if ! tags_js="$(curl -fsSL "$page_url")"; then
             echo "curl $page_url failed" >&2
             return 1
         fi
