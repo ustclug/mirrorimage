737809a9f311a3981a0130b56174f2161e5387f8
taoky <me@taoky.moe>
2024-01-22 15:29:16 +0800

    Add check to curl in common.sh

--- common.sh
+++ common.sh
@@ -3,10 +3,18 @@
 docker-tags(){
     image=library/$1
     tags_js=$(curl -sSL "https://registry.hub.docker.com/v2/repositories/${image}/tags/")
-    grep -oP '(?<="name":").+?(?=")' <(echo $tags_js)
-    while next_page=$(grep -oP '(?<="next":").+?(?=")' <(echo $tags_js) | sed 's/\\u0026/\&/' )
+    if [ $? -ne 0 ]; then
+        echo "curl $image failed" >&2
+        return 1
+    fi
+    grep -oP '(?<="name":").+?(?=")' <(echo "$tags_js")
+    while next_page=$(grep -oP '(?<="next":").+?(?=")' <(echo "$tags_js") | sed 's/\\u0026/\&/' )
     do
         tags_js=$(curl -sSL "$next_page")
-        grep -oP '(?<="name":").+?(?=")' <(echo $tags_js)
+        if [ $? -ne 0 ]; then
+            echo "curl $next_page failed" >&2
+            return 1
+        fi
+        grep -oP '(?<="name":").+?(?=")' <(echo "$tags_js")
     done
 }
