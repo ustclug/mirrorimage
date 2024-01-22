b3f5d77026ab604003a1e85115d22642d96ec9d7
cssc <754396291@qq.com>
2024-01-22 20:08:29 +0800

    Add function to parse and filter Docker tags by date

--- common.sh
+++ common.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+one_year_ago=$(date -d "1 year ago" +%s)
+parse_tags_js() {
+    is_empty=true
+    local date_names=($(grep -oP '(?<="last_updated":").+?(?=")|(?<="name":").+?(?=")' <(echo "$tags_js")))
+    for ((i=0; i<${#date_names[@]}/2; i++)); do
+        date=$(date -d "${date_names[2*i]}" +%s)
+        name=${date_names[2*i+1]}
+        if (( date > one_year_ago )); then
+            echo "$name"
+            is_empty=false
+    fi
+    done
+}
 docker-tags(){
     image=library/$1
     tags_js=$(curl -sSL "https://registry.hub.docker.com/v2/repositories/${image}/tags/")
@@ -7,10 +20,10 @@ docker-tags(){
         echo "curl $image failed" >&2
         return 1
     fi
-    grep -oP '(?<="name":").+?(?=")' <(echo "$tags_js")
+    parse_tags_js
     while next_page=$(grep -oP '(?<="next":").+?(?=")' <(echo "$tags_js") | sed 's/\\u0026/\&/' | xargs)
     do
-        if [[ -z "$next_page" || "$next_page" == "null" ]]; then
+        if [[ -z "$next_page" || "$next_page" == "null" || $is_empty == true ]]; then
             break
         fi
         tags_js=$(curl -sSL "$next_page")
@@ -18,6 +31,6 @@ docker-tags(){
             echo "curl $next_page failed" >&2
             return 1
         fi
-        grep -oP '(?<="name":").+?(?=")' <(echo "$tags_js")
+        parse_tags_js
     done
 }
