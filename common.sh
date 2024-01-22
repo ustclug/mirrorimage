67e86f8d11c99c85070a2a06aa016a475a4230be
taoky <me@taoky.moe>
2024-01-22 15:09:55 +0800

    Escape \u0026 (&) in URL
    Resolve https://github.com/ustclug/mirrorimage/actions/runs/7604242612

--- common.sh
+++ common.sh
@@ -4,9 +4,9 @@ docker-tags(){
     image=library/$1
     tags_js=$(curl -sSL "https://registry.hub.docker.com/v2/repositories/${image}/tags/")
     grep -oP '(?<="name":").+?(?=")' <(echo $tags_js)
-    while next_page=$(grep -oP '(?<="next":").+?(?=")' <(echo $tags_js) )
+    while next_page=$(grep -oP '(?<="next":").+?(?=")' <(echo $tags_js) | sed 's/\\u0026/\&/' )
     do
-        tags_js=$(curl -sSL $next_page)
+        tags_js=$(curl -sSL "$next_page")
         grep -oP '(?<="name":").+?(?=")' <(echo $tags_js)
     done
 }
