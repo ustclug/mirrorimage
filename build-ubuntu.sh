b481a1975584f6ac736119ca8a79069b092473b9
taoky <me@taoky.moe>
2024-05-11 02:44:44 +0800

    ubuntu: fix sed command
    /etc/apt/sources.list always exists, so OR (||) won't work.

--- build-ubuntu.sh
+++ build-ubuntu.sh
@@ -9,11 +9,11 @@ FROM ubuntu:$tag
 RUN sed -i \
     -e 's/archive.ubuntu.com/mirrors.ustc.edu.cn/g' \
     -e 's/security.ubuntu.com/mirrors.ustc.edu.cn/g' \
-    /etc/apt/sources.list || \
+    /etc/apt/sources.list; \
     sed -i \
     -e 's/archive.ubuntu.com/mirrors.ustc.edu.cn/g' \
     -e 's/security.ubuntu.com/mirrors.ustc.edu.cn/g' \
-    /etc/apt/sources.list.d/ubuntu.sources
+    /etc/apt/sources.list.d/ubuntu.sources || true
 EOF
     docker build -f $dockerfile -t ustclug/ubuntu:$tag .
     docker push ustclug/ubuntu:$tag
